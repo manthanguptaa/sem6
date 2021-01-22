8c4ff1634a8882762b791a3334336172cdab38ea
Manthan Gupta <manthangupta109@gmail.com>
2021-01-22 22:31:21 +0530

    Lab2 Q1

---
+++ C++/E18CSE102_Lab2/E18CSE102_Lab2_Q1.cpp
@@ -0,0 +1,14 @@
+//Manthan Gupta - E18CSE102
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter Input Number: ";
+    cin>>n;
+    cout<<"Square: "<<(n*n)<<"\n";
+    cout<<"Cube: "<<(n*n*n)<<"\n";
+    return 0;
+}

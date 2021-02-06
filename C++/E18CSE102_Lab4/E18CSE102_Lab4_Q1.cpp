40794e070adca072b4f7e67de1bbb63a84120655
Manthan Gupta <manthangupta109@gmail.com>
2021-02-06 21:09:09 +0530

    Q1 fixed

--- C++/E18CSE102_Lab4/E18CSE102_Lab4_Q1.cpp
+++ C++/E18CSE102_Lab4/E18CSE102_Lab4_Q1.cpp
@@ -1,15 +1,25 @@
 #E18CSE102 Manthan Gupta EB03
 #include <iostream>
-
+#define INT_MIN -1000000007
+#include <vector>
 using namespace std;
 
 void pairwiseSum(int arr[], int brr[], int n, int m){
+    vector<int> v;
+    int res=INT_MIN;
     for(int i=0;i<n;++i){
         for(int j=0;j<m;++j){
             cout<<arr[i]+brr[j]<<" ";
+            v.push_back(arr[i]+brr[j]);
         }
     }
     cout<<"\n";
+    for(int i=0;i<v.size()-1;++i){
+        for(int j=i+1;j<v.size();++j){
+            res=max(res,v[i]^v[j]);
+        }
+    }
+    cout<<"MAXPAIR: "<<res; 
 }
 
 int main()
